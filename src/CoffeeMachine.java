0d6c16c0a91814d7babe60321a34abff01e01628
Danny <64429863+PhenixFine@users.noreply.github.com>
2021-04-10 18:38:16 -0400

    Stage 2 of 6 for JetBrains Academy - Java - Coffee Machine project. • has us print out how many of each ingredient will be needed for user specified cups of coffee.

--- src/CoffeeMachine.java
+++ src/CoffeeMachine.java
@@ -1,11 +1,17 @@
+import java.util.Scanner;
+
 public class CoffeeMachine {
     public static void main(String[] args) {
-        System.out.println("Starting to make a coffee\n" +
-                "Grinding coffee beans\n" +
-                "Boiling water\n" +
-                "Mixing boiled water with crushed coffee beans\n" +
-                "Pouring coffee into the cup\n" +
-                "Pouring some milk into the cup\n" +
-                "Coffee is ready!");
+        int cups = getNum("Write how many cups of coffee you will need:");
+
+        System.out.printf("For %d cups of coffee you will need:%n", cups);
+        System.out.printf("%d ml of water%n", cups * 200);
+        System.out.printf("%d ml of milk%n", cups * 50);
+        System.out.printf("%d g of coffee beans%n", cups * 15);
+    }
+
+    public static int getNum(String message) {
+        System.out.println(message);
+        return new Scanner(System.in).nextInt();
     }
 }
