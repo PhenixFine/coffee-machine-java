1dd34529acffdf341d6a839b3a06c3f5304ef016
Danny <64429863+PhenixFine@users.noreply.github.com>
2021-04-10 22:25:31 -0400

    Stage 3 of 6 for JetBrains Academy - Java - Coffee Machine project. • this stage has the user fill up the coffee machine and then asks the user the amount of cups of coffee needed. The appropriate message is printed depending on if there's enough to make the amount of coffee the user requested. • main() - adjusted to call fillMachine() and makeCoffee() • fillMachine() - sets the top level values of water, milk, and beans by requesting them from the user, using getNum() • makeCoffee() - asks user for cups of coffee required and then sets the most cups that can be made with current supplies, printing 3 different messages to the user depending on if the requested cups of coffee can be made. • getNum() - adjusted from last stage to user the top level declared SCAN

--- src/CoffeeMachine.java
+++ src/CoffeeMachine.java
@@ -1,17 +1,35 @@
 import java.util.Scanner;
 
 public class CoffeeMachine {
-    public static void main(String[] args) {
-        int cups = getNum("Write how many cups of coffee you will need:");
+    private static final Scanner SCAN = new Scanner(System.in);
+    private static int WATER = 0;
+    private static int MILK = 0;
+    private static int BEANS = 0;
 
-        System.out.printf("For %d cups of coffee you will need:%n", cups);
-        System.out.printf("%d ml of water%n", cups * 200);
-        System.out.printf("%d ml of milk%n", cups * 50);
-        System.out.printf("%d g of coffee beans%n", cups * 15);
+    public static void main(String[] args) {
+        fillMachine();
+        makeCoffee();
     }
 
-    public static int getNum(String message) {
+    private static void fillMachine() {
+        String message = "Write how many %s of %s the coffee machine has:";
+
+        WATER = getNum(String.format(message, "ml", "water"));
+        MILK = getNum(String.format(message, "ml", "milk"));
+        BEANS = getNum(String.format(message, "grams", "coffee beans"));
+    }
+
+    private static void makeCoffee() {
+        int cups = getNum("Write how many cups of coffee you will need:");
+        int mostCups = Math.min(Math.min(WATER / 200, MILK / 50), BEANS / 15);
+
+        System.out.println(cups == mostCups ? "Yes, I can make that amount of coffee" : cups > mostCups ?
+                String.format("No, I can make only %d cup(s) of coffee", mostCups) :
+                String.format("Yes, I can make that amount of coffee (and even %d more than that)", mostCups - cups));
+    }
+
+    private static int getNum(String message) {
         System.out.println(message);
-        return new Scanner(System.in).nextInt();
+        return SCAN.nextInt();
     }
 }
