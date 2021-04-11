5e95d386d7f2140d2a9376685983b555a81cfb94
Danny <64429863+PhenixFine@users.noreply.github.com>
2021-04-11 18:13:14 -0400

    Stage 4 of 6 for JetBrains Academy - Java - Coffee Machine project. • this stage has us offer three types of drinks to the user. The user can chose between commands buy, fill, and take. If the user picks buy, they are given the option of 1 for espresso, 2 for latte, and 3 for cappuccino. If they select fill, that are asked how much to add for water, milk, beans, and cups. If they select take that are given the current amount of money and the money is reset to zero. • main() - adjusted to call inventory(), and then request the command from the user, calling the appropriate function depending on given command, and then calling inventory() again. •  fillMachine() - adjusted to add to water, milk, beans, and cups. And message was adjusted to changes to this stages requirements. • makeCoffee() was renamed buyCoffee() - most of the original content was removed for this stage requirements of allowing user to buy one of three drinks. The inventory is adjusted dependent on the user's choice, assuming there is enough inventory to make the drink ( not enough resources would print otherwise, which won't actually happen for this stage, given it doesn't allow for the purchase of more than one drink ). • takeMoney() - prints that it gave the user the current amount of money and then resets the value of money to zero.

--- src/CoffeeMachine.java
+++ src/CoffeeMachine.java
@@ -2,30 +2,73 @@ import java.util.Scanner;
 
 public class CoffeeMachine {
     private static final Scanner SCAN = new Scanner(System.in);
-    private static int WATER = 0;
-    private static int MILK = 0;
-    private static int BEANS = 0;
+    private static int WATER = 400;
+    private static int MILK = 540;
+    private static int BEANS = 120;
+    private static int MONEY = 550;
+    private static int CUPS = 9;
+
+    // 0 - water, 1 - milk, 2 - beans, 3 - money
+    private static final int[] ESPRESSO = {250, 0, 16, 4};
+    private static final int[] LATTE = {350, 75, 20, 7};
+    private static final int[] CAPPUCCINO = {200, 100, 12, 6};
 
     public static void main(String[] args) {
-        fillMachine();
-        makeCoffee();
+        inventory();
+        System.out.println("\nWrite action (buy, fill, take): ");
+        switch (SCAN.next().toLowerCase()) {
+            case "buy":
+                buyCoffee();
+                break;
+            case "fill":
+                fillMachine();
+                break;
+            case "take":
+                takeMoney();
+                break;
+            default:
+                return;
+        }
+        System.out.println();
+        inventory();
+    }
+
+    private static void inventory() {
+        System.out.println("The coffee machine has:\n" +
+                WATER + " of water\n" +
+                MILK + " of milk\n" +
+                BEANS + " of coffee beans\n" +
+                CUPS + " of disposable cups\n" +
+                MONEY + " of money");
     }
 
     private static void fillMachine() {
-        String message = "Write how many %s of %s the coffee machine has:";
+        String message = "Write how many %s of %s do you want to add:";
 
-        WATER = getNum(String.format(message, "ml", "water"));
-        MILK = getNum(String.format(message, "ml", "milk"));
-        BEANS = getNum(String.format(message, "grams", "coffee beans"));
+        WATER += getNum(String.format(message, "ml", "water"));
+        MILK += getNum(String.format(message, "ml", "milk"));
+        BEANS += getNum(String.format(message, "grams", "coffee beans"));
+        CUPS += getNum(String.format(message, "disposable cups", "coffee"));
     }
 
-    private static void makeCoffee() {
-        int cups = getNum("Write how many cups of coffee you will need:");
-        int mostCups = Math.min(Math.min(WATER / 200, MILK / 50), BEANS / 15);
+    private static void buyCoffee() {
+        int choice = getNum("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:");
+        if (choice < 1 || choice > 3) return;
+        int[] drink = choice == 1 ? ESPRESSO : choice == 2 ? LATTE : CAPPUCCINO;
+        int cups = Math.min(WATER / drink[0], Math.min(MILK / Math.max(drink[1], 1), Math.min(BEANS / drink[2], CUPS)));
 
-        System.out.println(cups == mostCups ? "Yes, I can make that amount of coffee" : cups > mostCups ?
-                String.format("No, I can make only %d cup(s) of coffee", mostCups) :
-                String.format("Yes, I can make that amount of coffee (and even %d more than that)", mostCups - cups));
+        if (cups > 0) {
+            WATER -= drink[0];
+            MILK -= drink[1];
+            BEANS -= drink[2];
+            MONEY += drink[3];
+            CUPS--;
+        } else System.out.println("Not enough resources");
+    }
+
+    private static void takeMoney() {
+        System.out.printf("I gave you $%d%n", MONEY);
+        MONEY = 0;
     }
 
     private static int getNum(String message) {
