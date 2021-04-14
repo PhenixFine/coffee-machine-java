fec647cbf2244cb76430856306560f4e8638f5d2
Danny <64429863+PhenixFine@users.noreply.github.com>
2021-04-14 19:23:46 -0400

    Stage 5 of 6 for JetBrains Academy - Java - Coffee Machine project. • this stage expanded the menu, adding remaining and exit; and loops the menu until the user types exit. • main() - adjusted it to loop until the user types exit, and adjusted it to use getString(), and moved the calling of inventory() to when the user types remaining. • inventory() - adjusted to print a new line first, and added dollar sign. • buyCoffee() - Moved part of it into getCoffee() and added the option back, if the user changed their mind about getting a drink. It now just loops until the user enters 1, 2, 3, or back. Calling getCoffee() with the number if the user types 1, 2, or 3. • getCoffee() - sets the drink choice to the correct array. Sets String outOf to the first item that there isn't enough of ( and to empty if there is enough items ). If outOf is empty it will confirm to the user that it is making a coffee, and then deduct the resources from inventory and add to money the cost of the drink. If outOf is not empty, it prints to the user that there wasn't enough of the first item that there wasn't enough of. • getString() - added to get a string from the user, cast to lowercase.

--- src/CoffeeMachine.java
+++ src/CoffeeMachine.java
@@ -14,32 +14,33 @@ public class CoffeeMachine {
     private static final int[] CAPPUCCINO = {200, 100, 12, 6};
 
     public static void main(String[] args) {
-        inventory();
-        System.out.println("\nWrite action (buy, fill, take): ");
-        switch (SCAN.next().toLowerCase()) {
-            case "buy":
-                buyCoffee();
-                break;
-            case "fill":
-                fillMachine();
-                break;
-            case "take":
-                takeMoney();
-                break;
-            default:
-                return;
+        while (true) {
+            switch (getString("Write action (buy, fill, take, remaining, exit):")) {
+                case "exit":
+                    return;
+                case "buy":
+                    buyCoffee();
+                    break;
+                case "fill":
+                    fillMachine();
+                    break;
+                case "take":
+                    takeMoney();
+                    break;
+                case "remaining":
+                    inventory();
+            }
+            System.out.println();
         }
-        System.out.println();
-        inventory();
     }
 
     private static void inventory() {
-        System.out.println("The coffee machine has:\n" +
+        System.out.println("\nThe coffee machine has:\n" +
                 WATER + " of water\n" +
                 MILK + " of milk\n" +
                 BEANS + " of coffee beans\n" +
                 CUPS + " of disposable cups\n" +
-                MONEY + " of money");
+                "$" + MONEY + " of money");
     }
 
     private static void fillMachine() {
@@ -52,23 +53,41 @@ public class CoffeeMachine {
     }
 
     private static void buyCoffee() {
-        int choice = getNum("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:");
-        if (choice < 1 || choice > 3) return;
-        int[] drink = choice == 1 ? ESPRESSO : choice == 2 ? LATTE : CAPPUCCINO;
-        int cups = Math.min(WATER / drink[0], Math.min(MILK / Math.max(drink[1], 1), Math.min(BEANS / drink[2], CUPS)));
+        String command;
 
-        if (cups > 0) {
+        do {
+            command = getString("\nWhat do you want to buy? 1 - espresso, 2 - latte, " +
+                    "3 - cappuccino, back - to main menu:");
+            if (command.length() == 1 && "123".contains(command)) {
+                getCoffee(Integer.parseInt(command));
+                return;
+            }
+        } while (!command.equals("back"));
+    }
+
+    private static void getCoffee(int choice) {
+        int[] drink = choice == 1 ? ESPRESSO : choice == 2 ? LATTE : CAPPUCCINO;
+        String outOf = CUPS < 1 ? "disposable cups" : WATER < drink[0] ? "water" : MILK < drink[1] ? "milk" :
+                BEANS < drink[2] ? "coffee beans" : "";
+
+        if (outOf.length() == 0) {
+            System.out.println("I have enough resources, making you a coffee!");
             WATER -= drink[0];
             MILK -= drink[1];
             BEANS -= drink[2];
             MONEY += drink[3];
             CUPS--;
-        } else System.out.println("Not enough resources");
+        } else System.out.printf("Sorry, not enough %s!%n", outOf);
     }
 
     private static void takeMoney() {
         System.out.printf("I gave you $%d%n", MONEY);
         MONEY = 0;
+    }
+
+    private static String getString(String message) {
+        System.out.println(message);
+        return SCAN.next().toLowerCase();
     }
 
     private static int getNum(String message) {
