5af0556990b490f79268339daa9a88d6ce6007d6
Danny <64429863+PhenixFine@users.noreply.github.com>
2021-05-19 21:14:31 -0400

    Stage 6 of 6 for JetBrains Academy - Java - Coffee Machine project. CoffeeMachine.java: • this stage has us change the coffee machine so that it stores it current state and works more like a machine would, and doesn't handle getting the input from the user. • fields STATUS, BUY, and FILL were added to keep track of the current state of the machine. Arrays ESPRESSO, LATTE, and CAPPUCCINO were removed to instead make use of enum Drink. • main() - was moved to Main.java to handle creating an object of CoffeeMachine.java and passing input from the user to the coffee machine. • run() - if the machine is on(), it checking and/or retrieving the current state from the user and calling the appropriate method dependant on the state it is in ( or returning if its current state is exit ). • start() - can be used to turn on the coffee machine ( changes state from exit to start ), and also for methods that are done processing to reset the state to the main menu option. • on() - returns true if the coffee machine's current state is not exit. • buyCoffee() - adjusted to use a switch to check the current state of BUY, printing the menu option if the state is START, and then checking the given user input if the state is COMMAND, only exiting and resetting its state and the STATUS when either a drink choice is picked, or the command "back" is typed. • getCoffee() - adjusted to use Enum Drink to retrieve a specific drink dependant on the user's choice it received. • fillMachine() - adjusted to check the state of FILL using a switch, that cycles through the items needing to be filled before resetting its state and the STATUS state ( if the user doesn't type in a number when needed, it returns back until it receives a number from the user to proceed ). • inventory() - adjusted to use String.format • isNumber() - returns true if given string is a number, using a try-catch block that attempts to cast the string to a number. • print() - created to shorten the typing of System.out.println • getString - was removed • getNum - was removed

--- src/CoffeeMachine.java
+++ src/CoffeeMachine.java
@@ -1,97 +1,136 @@
-import java.util.Scanner;
-
 public class CoffeeMachine {
-    private static final Scanner SCAN = new Scanner(System.in);
-    private static int WATER = 400;
-    private static int MILK = 540;
-    private static int BEANS = 120;
-    private static int MONEY = 550;
-    private static int CUPS = 9;
+    private CurrentState STATUS = CurrentState.EXIT;
+    private CurrentState.BUYING BUY = CurrentState.BUYING.START;
+    private CurrentState.FILLING FILL = CurrentState.FILLING.START;
+    private int WATER = 400;
+    private int MILK = 540;
+    private int BEANS = 120;
+    private int MONEY = 550;
+    private int CUPS = 9;
 
-    // 0 - water, 1 - milk, 2 - beans, 3 - money
-    private static final int[] ESPRESSO = {250, 0, 16, 4};
-    private static final int[] LATTE = {350, 75, 20, 7};
-    private static final int[] CAPPUCCINO = {200, 100, 12, 6};
-
-    public static void main(String[] args) {
-        while (true) {
-            switch (getString("Write action (buy, fill, take, remaining, exit):")) {
-                case "exit":
+    public void run(String input) {
+        if (on()) {
+            if (STATUS == CurrentState.START) STATUS = CurrentState.getState(input);
+            switch (STATUS) {
+                case START:
+                    start();
                     return;
-                case "buy":
-                    buyCoffee();
-                    break;
-                case "fill":
-                    fillMachine();
-                    break;
-                case "take":
+                case EXIT:
+                    return;
+                case BUY:
+                    buyCoffee(input);
+                    return;
+                case FILL:
+                    fillMachine(input);
+                    return;
+                case TAKE:
                     takeMoney();
-                    break;
-                case "remaining":
+                    return;
+                case REMAINING:
                     inventory();
             }
-            System.out.println();
         }
     }
 
-    private static void inventory() {
-        System.out.println("\nThe coffee machine has:\n" +
-                WATER + " of water\n" +
-                MILK + " of milk\n" +
-                BEANS + " of coffee beans\n" +
-                CUPS + " of disposable cups\n" +
-                "$" + MONEY + " of money");
+    public void start() {
+        if (STATUS != CurrentState.START) STATUS = CurrentState.START;
+        print("Write action (buy, fill, take, remaining, exit):");
     }
 
-    private static void fillMachine() {
-        String message = "Write how many %s of %s do you want to add:";
-
-        WATER += getNum(String.format(message, "ml", "water"));
-        MILK += getNum(String.format(message, "ml", "milk"));
-        BEANS += getNum(String.format(message, "grams", "coffee beans"));
-        CUPS += getNum(String.format(message, "disposable cups", "coffee"));
+    public boolean on() {
+        return STATUS != CurrentState.EXIT;
     }
 
-    private static void buyCoffee() {
-        String command;
-
-        do {
-            command = getString("\nWhat do you want to buy? 1 - espresso, 2 - latte, " +
-                    "3 - cappuccino, back - to main menu:");
-            if (command.length() == 1 && "123".contains(command)) {
-                getCoffee(Integer.parseInt(command));
+    private void buyCoffee(String order) {
+        String msg = "\nWhat do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:";
+        switch (BUY) {
+            case START:
+                print(msg);
+                BUY = CurrentState.BUYING.COMMAND;
                 return;
-            }
-        } while (!command.equals("back"));
+            case COMMAND:
+                if (order.length() == 1 && "123".contains(order)) {
+                    getCoffee(Integer.parseInt(order));
+                } else if (!"back".equalsIgnoreCase(order)) {
+                    print(msg);
+                    return;
+                }
+        }
+        print("");
+        BUY = CurrentState.BUYING.START;
+        start();
     }
 
-    private static void getCoffee(int choice) {
-        int[] drink = choice == 1 ? ESPRESSO : choice == 2 ? LATTE : CAPPUCCINO;
-        String outOf = CUPS < 1 ? "disposable cups" : WATER < drink[0] ? "water" : MILK < drink[1] ? "milk" :
-                BEANS < drink[2] ? "coffee beans" : "";
+    private void getCoffee(int choice) {
+        Drink drink = (choice == 1) ? Drink.ESPRESSO : (choice == 2) ? Drink.LATTE : Drink.CAPPUCCINO;
+        String outOf = (CUPS < 1) ? "disposable cups" : (WATER < drink.water) ? "water"
+                : (MILK < drink.milk) ? "milk" : (BEANS < drink.beans) ? "coffee beans" : "";
 
         if (outOf.length() == 0) {
-            System.out.println("I have enough resources, making you a coffee!");
-            WATER -= drink[0];
-            MILK -= drink[1];
-            BEANS -= drink[2];
-            MONEY += drink[3];
+            print("I have enough resources, making you a coffee!");
+            WATER -= drink.water;
+            MILK -= drink.milk;
+            BEANS -= drink.beans;
+            MONEY += drink.money;
             CUPS--;
-        } else System.out.printf("Sorry, not enough %s!%n", outOf);
+        } else print(String.format("Sorry, not enough %s!", outOf));
     }
 
-    private static void takeMoney() {
-        System.out.printf("I gave you $%d%n", MONEY);
+    private void fillMachine(String number) {
+        if (number == null || (FILL != CurrentState.FILLING.START && !isNumber(number))) return;
+        String message = "Write how many %s of %s do you want to add:";
+        int num = (FILL != CurrentState.FILLING.START) ? Integer.parseInt(number) : 0;
+
+        switch (FILL) {
+            case START:
+                FILL = CurrentState.FILLING.WATER;
+                print(String.format("\n" + message, "ml", "water"));
+                return;
+            case WATER:
+                WATER += num;
+                FILL = CurrentState.FILLING.MILK;
+                print(String.format(message, "ml", "milk"));
+                return;
+            case MILK:
+                MILK += num;
+                FILL = CurrentState.FILLING.BEANS;
+                print(String.format(message, "grams", "coffee beans"));
+                return;
+            case BEANS:
+                BEANS += num;
+                FILL = CurrentState.FILLING.CUPS;
+                print(String.format(message, "disposable cups", "coffee"));
+                return;
+            case CUPS:
+                CUPS += num;
+                FILL = CurrentState.FILLING.START;
+                print("");
+                start();
+        }
+    }
+
+    private void takeMoney() {
+        print(String.format("%nI gave you $%d%n", MONEY));
         MONEY = 0;
+        start();
     }
 
-    private static String getString(String message) {
-        System.out.println(message);
-        return SCAN.next().toLowerCase();
+    private void inventory() {
+        print(String.format("%nThe coffee machine has:%n%d ml of water%n%d ml of milk%n%d g of coffee beans%n" +
+                "%d disposable cups%n$%d of money%n", WATER, MILK, BEANS, CUPS, MONEY));
+        start();
     }
 
-    private static int getNum(String message) {
+    private boolean isNumber(String number) {
+        try {
+            Integer.parseInt(number);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private void print(String message) {
         System.out.println(message);
-        return SCAN.nextInt();
     }
 }
